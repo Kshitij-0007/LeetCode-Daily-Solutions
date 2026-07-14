047546f4e600ca4e76fe424cbe603fcc99f7fa9a
Kshitij Shivprasad Gawankar <179899651+Kshitij-0007@users.noreply.github.com>
2026-07-14 23:12:54 +0530

    Time: 0 ms (100%), Space: 8.1 MB (48.96%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -3,8 +3,7 @@ public:
     bool rotateString(string s, string goal) {
         if(s.size()!=goal.size())
             return false;
-        string nwStr=s.append(s);
-        return nwStr.find(goal)!=string::npos;
+        return (s+s).find(goal)!=string::npos;
 
     }
 };
