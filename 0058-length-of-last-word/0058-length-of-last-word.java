0ced1d0dff883cbf7e98f7cd0052efdcb5465979
Kshitij Shivprasad Gawankar <179899651+Kshitij-0007@users.noreply.github.com>
2026-07-28 23:25:02 +0530

    Time: 0 ms (100%), Space: 43.3 MB (6.43%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String s1=s.trim();
+        if(s1.charAt(0)==' ')
+            return 0;
+        int lstLen=0;
+        for(int i=s1.length()-1;i>=0;i--)
+        {
+            if(s1.charAt(i)==' ')
+                return lstLen;
+            lstLen++;
+        }
+        return lstLen;
+    }
+}
