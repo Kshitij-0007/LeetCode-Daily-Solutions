003996dbbd73bdd928daf46cdced9fb133dcb8d5
Kshitij Shivprasad Gawankar <179899651+Kshitij-0007@users.noreply.github.com>
2026-08-19 22:32:25 +0530

    Time: 0 ms (100%), Space: 8.9 MB (37.83%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,30 +3,30 @@ public:
     bool isValid(string s) {
         stack<char> stk;
 
-for (char ch : s)
-{
-    if (ch == '(' || ch == '[' || ch == '{')
-    {
-        stk.push(ch);
-    }
-    else
-    {
-        if (stk.empty())
-            return false;
-
-        if ((stk.top() == '(' && ch == ')') ||
-            (stk.top() == '[' && ch == ']') ||
-            (stk.top() == '{' && ch == '}'))
+        for (char ch : s)
         {
-            stk.pop();
-        }
-        else
-        {
-            return false;
-        }
-    }
-}
+            if (ch == '(' || ch == '[' || ch == '{')
+            {
+                stk.push(ch);
+            }
+            else
+            {
+                if (stk.empty())
+                    return false;
 
-return stk.empty();
+                if ((stk.top() == '(' && ch == ')') ||
+                    (stk.top() == '[' && ch == ']') ||
+                    (stk.top() == '{' && ch == '}'))
+                {
+                    stk.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        return stk.empty();
     }
 };
