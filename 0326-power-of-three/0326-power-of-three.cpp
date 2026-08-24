2a5821f28d0058d6e2894721662dd157c4948911
Kshitij Shivprasad Gawankar <179899651+Kshitij-0007@users.noreply.github.com>
2026-08-24 21:58:49 +0530

    Time: 4 ms (45.99%), Space: 8.9 MB (43.84%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -3,15 +3,10 @@ public:
     bool isPowerOfThree(int n) {
         if(n==1||n==3)
             return true;
-        if(n<3)
+        if(n<3 || n%3!=0)
         {
             return false;
         }
-        if(n%3!=0)
-        {
-            return false;
-        }
-        
         return isPowerOfThree(n/3);
     }
 };
