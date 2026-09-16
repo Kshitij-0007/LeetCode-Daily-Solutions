b26a2f68efcc58be9fe5206a88efb2270349dec0
Kshitij Shivprasad Gawankar <179899651+Kshitij-0007@users.noreply.github.com>
2026-09-16 20:23:13 +0530

    Time: 0 ms (100%), Space: 11.2 MB (90.46%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.c
+++ 0206-reverse-linked-list/0206-reverse-linked-list.c
@@ -7,16 +7,19 @@
  */
 struct ListNode* reverseList(struct ListNode* head) {
     if(head==NULL||head->next==NULL)
+    {
         return head;
+    }
     struct ListNode* p=head;
     struct ListNode* q=NULL;
     struct ListNode* temp=NULL;
-    while(p!=NULL)
-    {
+    while(p!=NULL){
         temp=p->next;
         p->next=q;
         q=p;
         p=temp;
     }
     return q;
+
+
 }
